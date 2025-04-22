bdcdda430a2318d3dd1ffd0e061656d34359b01b
erinwallace_89@hotmail.com <erinwallace_89@hotmail.com>
2025-04-22 19:37:45 +0000

    cleaned

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,7 +11,7 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        log_cost,
+        (logcost) as log_cost,
         cast((ship_cost) as float64) as ship_cost
 
     from source
