3cdd670e79ffc46e86cf0c4e2444146fe2951e25
erinwallace_89@hotmail.com <erinwallace_89@hotmail.com>
2025-04-23 12:17:21 +0000

    materialized table

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,3 +1,4 @@
+{{ config(materialized='table') }}
 SELECT
      date_date
      ,COUNT(orders_id) AS nb_transactions
@@ -14,3 +15,4 @@ SELECT
  FROM {{ref("int_orders_operational")}}
  GROUP BY  date_date
  ORDER BY  date_date DESC
+ {{ config(materialized='table') }}
